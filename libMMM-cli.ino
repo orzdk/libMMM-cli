8c96f64835e4b0ae65665a394bbdd5fddcc7003c
Jesper Christensen <jeschrper@hotmail.com>
2019-11-22 15:21:24 +0100

    Update libMMM-cli.ino

--- libMMM-cli.ino
+++ libMMM-cli.ino
@@ -3,9 +3,9 @@ typedef union  {
      uint8_t  packet[4];
 } midiPacket_t;
 
-#define stsd (pk.packet[1] & 0xF0) 
 #define gatelow userUnits[u]->tGate.gate.lower
 #define gatehigh userUnits[u]->tGate.gate.upper
+#define TRANSFORMERS_PR_CHANNEL 2
 
 #include <libMMM.h>
 
@@ -25,22 +25,23 @@ struct userUnit {
 userUnit* userUnits[16];
 uint8_t userUnitCount = 0;
 
-void lib3m_AddUserUnit(uint8_t cmdCode, uint8_t x, uint8_t y, uint8_t z, uint8_t g_low, uint8_t g_high)
+void lib3m_AddUserUnit(uint8_t cmdCode, uint8_t x, uint8_t y, uint8_t z, uint8_t v, uint8_t lb, uint8_t ub)
 {
     userUnit* uu = (userUnit*)malloc(sizeof(userUnit)); 
     uu->cmdCode = cmdCode;
-    uu->tGate.gate.lower = g_low;
-    uu->tGate.gate.upper = g_high;
+    uu->tGate.gate.lower = lb;
+    uu->tGate.gate.upper = ub;
     uu->tParms.x = x;
     uu->tParms.y = y;
     uu->tParms.z = z;
+    uu->tParms.v = v;
     userUnits[userUnitCount++] = uu;   
 }
 
 void lib3m_ProcessEvent(midiPacket_t &pk)
 {            
     for (int u=0;u<userUnitCount;u++){
-        if ( BETWEEN(stsd,gatelow,gatehigh) || BETWEEN(pk.packet[1],gatelow,gatehigh) ){      
+        if ( BETWEEN(pk.packet[1],gatelow,gatehigh) ){      
            (transformerCommands[userUnits[u]->cmdCode].fnFn)(&pk, userUnits[u]->tParms);
         }
     }
@@ -57,12 +58,15 @@ void setup(){
   Serial.begin(9600);
   delay(3000);
  
-  lib3m_AddUserUnit(evm, stsCONTINUE, stsSTART, 0, stsCONTINUE, stsCONTINUE);
-  lib3m_AddUserUnit(gvo, 20         , 0       , 0, stsNOTEOFF , stsNOTEON);
-
-  midiPacket_t pk = { .packet = { 0x0, stsCONTINUE, 0x0, 0x0 } };
-  test(pk);
+  //lib3m_AddUserUnit(evm, stsCONTINUE, stsSTART, 0, stsCONTINUE, stsCONTINUE);
+  //midiPacket_t pk = { .packet = { 0x0, stsCONTINUE, 0x0, 0x0 } };
+  //test(pk);
   
+  lib3m_AddUserUnit(gtn, 20, 0, 0, 0, stsNOTEOFF , stsNOTEON);
+ 
   midiPacket_t pk2 = { .packet = { 0x0, stsNOTEON, 0x10, 0x10 } };
   test(pk2);  
+
+  midiPacket_t pk3 = { .packet = { 0x0, stsNOTEOFF, 0x10, 0x10 } };
+  test(pk3);  
 }
